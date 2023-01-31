7fa0794d228ba540ea65126fd3e2b70968a8a33a
melonges <mellonges@icloud.com>
2023-01-31 16:25:51 +0300

    upgrade performance in condition

--- tic-tac-toe.sol
+++ tic-tac-toe.sol
@@ -6,7 +6,7 @@ contract TicTacToe {
    function isSolved(int[3][] memory board ) public pure returns (int) {
     int has_zero = 0;
     
-    if (board[0][0] == board[1][1] && board[0][0] == board[2][2] && board[0][0] != 0) {
+    if (board[0][0] != 0 && board[0][0] == board[1][1] && board[0][0] == board[2][2]) {
      return board[0][0];
     }
     
@@ -15,12 +15,12 @@ contract TicTacToe {
           has_zero = 1;
         }
         
-          if (board[i][0] == board[i][1] && board[i][0] == board[i][2] && board[0][0] != 0) {
+          if (board[0][0] != 0 && board[i][0] == board[i][1] && board[i][0] == board[i][2]) {
               return board[i][0];
 
           }
 
-          if (board[0][i] == board[1][i] && board[0][i] == board[2][i] && board[0][0] != 0) {
+          if (board[0][0] != 0 && board[0][i] == board[1][i] && board[0][i] == board[2][i]) {
             return board[0][i];
           }
       }
